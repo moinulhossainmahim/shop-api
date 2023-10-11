94a64e38b6d965631fa9df6dfdeec90c0b958c40
mhossain2 <mhossain2@tibxtech.com>
2023-10-12 01:50:51 +0600

    PB-31: added sub category custom response

--- src/sub-categories/sub-categories.controller.ts
+++ src/sub-categories/sub-categories.controller.ts
@@ -7,6 +7,7 @@ import {
   Patch,
   Post,
   UseGuards,
+  UseInterceptors,
 } from '@nestjs/common';
 import { SubCategoriesService } from './sub-categories.service';
 import { SubCategory } from 'src/entity/SubCategory';
@@ -16,10 +17,15 @@ import { UserRole } from 'src/decorators/role.decorator';
 import { JwtAuthGuard } from 'src/guards/jwt-auth.guard';
 import { RoleGuard } from 'src/guards/role.guard';
 import { Role } from 'src/users/enums/role.enum';
+import { CreateApiResponse } from 'src/common/create-response.interface';
+import { ResponseInterceptor } from 'src/interceptors/response.interceptor';
+import { ApiGetResponse } from 'src/common/get-response.interface';
+import { ApiDeleteResponse } from 'src/common/delete-response.interface';
 
 @Controller('sub-categories')
 @UseGuards(JwtAuthGuard, RoleGuard)
 @UserRole(Role.Admin)
+@UseInterceptors(ResponseInterceptor)
 @ApiTags('sub-categories')
 export class SubCategoriesController {
   constructor(private readonly subCategoriesService: SubCategoriesService) {}
@@ -27,25 +33,25 @@ export class SubCategoriesController {
   @Post()
   async createSubCategory(
     @Body() subCategoryDto: SubCategoryDto,
-  ): Promise<SubCategory> {
+  ): Promise<CreateApiResponse<SubCategory>> {
     return this.subCategoriesService.createSubCategory(subCategoryDto);
   }
 
   @Patch('/:id')
   async updateSubCategory(
     @Param('id') id: string,
-    @Body() subCategoryDto: SubCategoryDto,
-  ): Promise<SubCategory> {
+    @Body() subCategoryDto: Partial<SubCategoryDto>,
+  ): Promise<CreateApiResponse<SubCategory>> {
     return this.subCategoriesService.updateSubCategoryById(id, subCategoryDto);
   }
 
   @Delete('/:id')
-  async deleteSubCategory(@Param('id') id: string): Promise<void> {
+  async deleteSubCategory(@Param('id') id: string): Promise<ApiDeleteResponse> {
     return this.subCategoriesService.deleteSubCategoryById(id);
   }
 
   @Get()
-  async getAllSubCategories(): Promise<SubCategory[]> {
+  async getAllSubCategories(): Promise<ApiGetResponse<SubCategory>> {
     return this.subCategoriesService.getAllSubCategories();
   }
 }
