f14c24b7a7a9fbe064b90c518377c123a3315b55
mhossain2 <mhossain2@tibxtech.com>
2023-10-29 04:46:01 +0600

    updated url

--- src/utils/data-source.ts
+++ src/utils/data-source.ts
@@ -4,15 +4,11 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 const config = new ConfigService();
 
 export const dataSourceOptions: DataSourceOptions = {
-  database: config.get('DB_NAME'),
   entities: ['dist/entity/*.js'],
-  host: config.get('DB_HOST'),
   migrations: ['dist/migration/*.js'],
-  password: config.get('DB_PASSWORD'),
-  port: config.get('DB_PORT'),
   type: 'mysql',
+  url: process.env.MYSQL_URL,
   synchronize: false,
-  username: config.get('DB_USERNAME'),
 };
 
 const dataSource = new DataSource(dataSourceOptions);
