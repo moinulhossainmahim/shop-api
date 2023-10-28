87503fccb8d30a6a2b43d455407d533f7be2b71f
mhossain2 <mhossain2@tibxtech.com>
2023-10-29 01:55:15 +0600

    PB-46: updated

--- src/utils/generate-tracking-no.ts
+++ src/utils/generate-tracking-no.ts
@@ -1,7 +1,4 @@
 export function generateTrackingNo() {
-  const randomDigits = Math.floor(Math.random() * 10000)
-    .toString()
-    .padStart(4, '0');
   const now = new Date();
   const year = now.getFullYear();
   const month = (now.getMonth() + 1).toString().padStart(2, '0');
@@ -9,6 +6,10 @@ export function generateTrackingNo() {
   const hours = now.getHours().toString().padStart(2, '0');
   const minutes = now.getMinutes().toString().padStart(2, '0');
   const seconds = now.getSeconds().toString().padStart(2, '0');
-  const trackingNumber = `${year}${month}${day}${hours}${minutes}${seconds}-${randomDigits}`;
+  const milliseconds = now.getMilliseconds().toString().padStart(3, '0');
+  const randomDigits = Math.floor(Math.random() * 10000)
+    .toString()
+    .padStart(4, '0');
+  const trackingNumber = `${year}${month}${day}${hours}${minutes}${seconds}${milliseconds}-${randomDigits}`;
   return trackingNumber;
 }
