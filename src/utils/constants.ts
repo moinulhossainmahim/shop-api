cb788659302cf29b38a76a95c2c76dbf31ca32c6
mhossain2 <moinulhossainmahim@gmail.com>
2023-11-08 01:51:54 +0600

    updated constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,10 @@
+import { ConfigService } from '@nestjs/config';
+
+const config = new ConfigService();
+
 export const UNSUPPORTED_FILE = 'unsupportedfile';
+
+const uploadFileUrl =
+  config.get('NODE_ENV') === 'production'
+    ? 'mysql://root:f6GG65hae4Ehad1H5A44ChGE1B3cFbHb@roundhouse.proxy.rlwy.net:46860/railway'
+    : 'http://localhost:3000';
