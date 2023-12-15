08b1c4e36b5e7a045de6fa67943482a4548531b1
mhossain2 <moinulhossainmahim@gmail.com>
2023-12-15 16:14:21 +0600

    updated

--- dist/utils/constants.js
+++ dist/utils/constants.js
@@ -5,6 +5,6 @@ const config_1 = require("@nestjs/config");
 const config = new config_1.ConfigService();
 exports.UNSUPPORTED_FILE = 'unsupportedfile';
 exports.uploadFileUrl = config.get('NODE_ENV') === 'production'
-    ? 'https://shop-api-production-ce05.up.railway.app'
+    ? 'https://shop-api-5sp7.onrender.com/'
     : 'http://localhost:3000';
 //# sourceMappingURL=constants.js.map
